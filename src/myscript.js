e1333a12e4738e3b20a7e7157209bf1a4317143a
Kartik saini <kartiksaini@cedcommerce.com>
2022-02-09 14:39:25 +0530

    Multiply Two Decimals with JavaScript

--- src/myscript.js
+++ src/myscript.js
@@ -65,4 +65,7 @@ const ourDecimal = 5.7;
 // Only change code below this line
 const myDecimal = 5.7;//Create Decimal Numbers with JavaScript
 
+const product = 2.0 * 2.5;//Multiply Two Decimals with JavaScript
+
+
 
