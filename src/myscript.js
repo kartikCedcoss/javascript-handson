2725b2ae7746e676c4ab3141c40359e91066758e
Kartik saini <kartiksaini@cedcommerce.com>
2022-02-09 14:36:24 +0530

    Decrement a Number with JavaScript

--- src/myscript.js
+++ src/myscript.js
@@ -55,4 +55,9 @@ let myVar = 87;
 // Only change code below this line
 myVar++; // Increment a Number with JavaScript
 
+let myVar = 11;
+
+// Only change code below this line
+myVar--;//Decrement a Number with JavaScript
+
 
