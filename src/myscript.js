9d72f9e47246b8561572186f5031b30feb459382
Kartik saini <kartiksaini@cedcommerce.com>
2022-02-09 13:26:51 +0530

    Assigning the Value of One Variable to Another

--- src/myscript.js
+++ src/myscript.js
@@ -4,3 +4,10 @@ my multi line comment*/ //Comment Your JavaScript Code
 var  myName; //Declare JavaScript Variables
 var a;
 a=7; //Storing Values with the Assignment Operator
+// Setup
+var a;
+a = 7;
+var b;
+
+// Only change code below this line
+b=a; //Assigning the Value of One Variable to Another
