74b7a79d5c32b0ea36e17a81636c88ec8e255b2e
Kartik saini <kartiksaini@cedcommerce.com>
2022-02-09 14:34:58 +0530

    Increment a Number with JavaScript

--- src/myscript.js
+++ src/myscript.js
@@ -49,3 +49,10 @@ const difference = 45 - 33;//Subtract One Number from Another with JavaScript
 const product = 8 * 10; //Multiply Two Numbers with JavaScript
 
 const quotient = 66 / 33;//Divide One Number by Another with JavaScript
+
+let myVar = 87;
+
+// Only change code below this line
+myVar++; // Increment a Number with JavaScript
+
+
