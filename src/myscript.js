adf28b3ce18fcb2fae5bd9a1e7b021969bb53b83
Kartik saini <kartiksaini@cedcommerce.com>
2022-02-09 17:19:14 +0530

    Comparison with the Inequality Operator

--- src/myscript.js
+++ src/myscript.js
@@ -373,4 +373,14 @@ function compareEquality(a, b) {
   }
   
   compareEquality(10, "10");//Practice comparing different values
+  // Setup
+function testNotEqual(val) {
+    if (val!=99) { // Change this line
+      return "Not Equal";
+    }
+    return "Equal";
+  }
+  
+  testNotEqual(10);//Comparison with the Inequality Operator
+
 
