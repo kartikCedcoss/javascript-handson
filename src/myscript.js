00f2b201b206ae0a222114c68fe507c03519adcc
Kartik saini <kartiksaini@cedcommerce.com>
2022-02-09 17:35:24 +0530

    Comparisons with the Logical And Operator

--- src/myscript.js
+++ src/myscript.js
@@ -448,3 +448,17 @@ function testStrictNotEqual(val) {
   }
   
   testLessOrEqual(10);//Comparison with the Less Than Or Equal To Operator
+  function testLogicalAnd(val) {
+    // Only change code below this line
+  
+    if (val <= 50 && val >=25) {
+      
+        return "Yes";
+      }
+    
+  
+    // Only change code above this line
+    return "No";
+  }
+  
+  testLogicalAnd(10);// Comparisons with the Logical And Operator
