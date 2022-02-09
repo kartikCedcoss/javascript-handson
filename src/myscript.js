3ccd010e73e41c19da36b518b3a2fd8f556486de
Kartik saini <kartiksaini@cedcommerce.com>
2022-02-09 16:07:40 +0530

    Manipulate Arrays With shift()

--- src/myscript.js
+++ src/myscript.js
@@ -206,3 +206,9 @@ const myArray = [["John", 23], ["cat", 2]];
 // Only change code below this line
 var removedFromMyArray = myArray.pop();//Manipulate Arrays With pop()
 
+// Setup
+const myArray = [["John", 23], ["dog", 3]];
+
+// Only change code below this line
+const removedFromMyArray =myArray.shift();//Manipulate Arrays With shift()
+
