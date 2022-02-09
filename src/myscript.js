5cabc053e6b8cbcdfb708e9a10c766f773da9d5a
Kartik saini <kartiksaini@cedcommerce.com>
2022-02-09 17:53:12 +0530

    Chaining If Else Statements

--- src/myscript.js
+++ src/myscript.js
@@ -522,3 +522,31 @@ function testStrictNotEqual(val) {
   }
   
   orderMyLogic(7);//Logical Order in If Else Statements
+  function testSize(num) {
+    // Only change code below this line
+   if(num<5){
+     return "Tiny";
+   }
+    else if(num<10){
+     return "Small";
+   }
+  
+   else if(num<15){
+     return  "Medium";
+   }
+  
+   else if(num<20){
+     return "Large";
+   }
+  
+   else {
+     return "Huge";
+   }
+  
+  
+    return "Change Me";
+    // Only change code above this line
+  }
+  
+  testSize(7);//Chaining If Else Statements
+
