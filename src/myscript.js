54c9644a12ffb4ef0984b34afe9abd747a985ad3
Kartik saini <kartiksaini@cedcommerce.com>
2022-02-09 14:13:16 +0530

    Understanding Uninitialized Variables

--- src/myscript.js
+++ src/myscript.js
@@ -15,3 +15,12 @@ var a=9;//Initializing Variables with the Assignment Operator
 
 var myFirstName = "Kartik";
 var myLastName = "Saini";//Declare String Variables
+// Only change code below this line
+var a=5;
+var b=10;
+var c="I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!"; //Understanding Uninitialized Variables
