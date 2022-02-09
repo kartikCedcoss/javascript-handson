46cf4da9c7a7371c00d36b97800467da4405d602
Kartik saini <kartiksaini@cedcommerce.com>
2022-02-09 17:41:35 +0530

    Introducing Else Statements

--- src/myscript.js
+++ src/myscript.js
@@ -476,3 +476,21 @@ function testStrictNotEqual(val) {
   }
   
   testLogicalOr(15);//Comparisons with the Logical Or Operator
+
+  function testElse(val) {
+    let result = "";
+    // Only change code below this line
+  
+    if (val > 5) {
+      result = "Bigger than 5";
+    }
+  
+    else {
+      result = "5 or Smaller";
+    }
+  
+    // Only change code above this line
+    return result;
+  }
+  
+  testElse(4);//Introducing Else Statements
