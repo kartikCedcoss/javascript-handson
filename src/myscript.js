98dd689ba0d6f8037cbd3b13b577b510e14d9e14
Kartik saini <kartiksaini@cedcommerce.com>
2022-02-09 16:47:15 +0530

    Understanding Undefined Value returned from a Function

--- src/myscript.js
+++ src/myscript.js
@@ -284,3 +284,21 @@ function myOutfit() {
 
 myOutfit();//Global vs. Local Scope in Functions
 
+// Setup
+let sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive(){
+  sum=sum+5;
+}
+
+// Only change code above this line
+
+addThree();
+addFive();//Understanding Undefined Value returned from a Function
+
+
