6c9b4ae1482435c0e151add824b1eb2427b895ad
Kartik saini <kartiksaini@cedcommerce.com>
2022-02-09 17:31:00 +0530

    Comparison with the Less Than Operator

--- src/myscript.js
+++ src/myscript.js
@@ -421,4 +421,16 @@ function testStrictNotEqual(val) {
   
   testGreaterOrEqual(10);//Comparison with the Greater Than Or Equal To Operator
 
-
+  function testLessThan(val) {
+    if (val<25) {  // Change this line
+      return "Under 25";
+    }
+  
+    if (val<55) {  // Change this line
+      return "Under 55";
+    }
+  
+    return "55 or Over";
+  }
+  
+  testLessThan(10);//  Comparison with the Less Than Operator
