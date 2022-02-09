c8d2bb55eb7652738b0ecff8be5267d14bf51c67
Kartik saini <kartiksaini@cedcommerce.com>
2022-02-09 16:31:07 +0530

    Global Scope and Functions

--- src/myscript.js
+++ src/myscript.js
@@ -239,3 +239,25 @@ function reusableFunction() {
   timesFive(2);
   timesFive(0);//Return a Value from a Function with Return
 
+  // Declare the myGlobal variable below this line
+let myGlobal=10;
+
+function fun1() {
+   oopsGlobal=5;
+  // Assign 5 to oopsGlobal Here
+
+}
+
+// Only change code above this line
+
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}// Global Scope and Functions
+
