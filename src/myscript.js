0dffd6a83a8d6dbb657450d47f812fa26f5249b3
Kartik saini <kartiksaini@cedcommerce.com>
2022-02-10 09:46:56 +0530

    Iterate Through an Array with a For Loop

--- src/myscript.js
+++ src/myscript.js
@@ -938,3 +938,13 @@ const myArray = [];
 for(let i=9; i>0; i-=2){
   myArray.push(i)
 }//Count Backwards With a For Loop
+
+// Setup
+const myArr = [2, 3, 4, 5, 6];
+
+// Only change code below this line
+var total=0;
+for(let i=0; i<myArr.length; i++)
+{
+  total+=myArr[i]
+} //Iterate Through an Array with a For Loop
