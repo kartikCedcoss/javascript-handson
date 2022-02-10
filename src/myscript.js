3f9417a6ed513d1e4941ab7e54a0bcba6b7d0703
Kartik saini <kartiksaini@cedcommerce.com>
2022-02-10 08:47:52 +0530

    Replacing If Else Chains with Switch

--- src/myscript.js
+++ src/myscript.js
@@ -664,3 +664,33 @@ function caseInSwitch(val) {
   }
   
   sequentialSizes(1);//Multiple Identical Options in Switch Statements
+
+  function chainToSwitch(val) {
+    let answer = "";
+    // Only change code below this line
+  
+    switch(val){
+      case "bob":
+      answer= "Marley";
+      break;
+      case 42:
+      answer="The Answer";
+      break;
+      case 1:
+      answer= "There is no #1";
+      break;
+      case 99:
+      answer="Missed me by this much!";
+      break;
+      case 7:
+      answer="Ate Nine";
+      break;
+  
+  
+    }
+    
+    // Only change code above this line
+    return answer;
+  }
+  
+  chainToSwitch(7);//Replacing If Else Chains with Switch
