8c90d2081fb39e6f0a652de10f70a672fe20e188
Kartik saini <kartiksaini@cedcommerce.com>
2022-02-10 08:57:34 +0530

    Using Objects for Lookups

--- src/myscript.js
+++ src/myscript.js
@@ -822,3 +822,21 @@ const myDog = {
 
 // Only change code below this line
 delete myDog.tails;//Delete Properties from a JavaScript Object
+
+function phoneticLookup(val) {
+  var result = "";
+
+  // Only change code below this line
+  
+  var lookup = {
+    "alpha": "Adams",
+    "bravo": "Boston",
+    "charlie": "Chicago",
+    "delta": "Denver",
+    "echo": "Easy",
+    "foxtrot": "Frank"
+  };
+  result = lookup[val];
+  // Only change code above this line
+  return result;
+}//Using Objects for Lookups  
