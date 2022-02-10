afc1415762f04c7da724752ba9f4bbbd702c4279
Kartik saini <kartiksaini@cedcommerce.com>
2022-02-10 09:51:58 +0530

    Nesting For Loops

--- src/myscript.js
+++ src/myscript.js
@@ -948,3 +948,17 @@ for(let i=0; i<myArr.length; i++)
 {
   total+=myArr[i]
 } //Iterate Through an Array with a For Loop
+
+function multiplyAll(arr) {
+  let product = 1;
+  // Only change code below this line
+for (let i = 0; i < arr.length; i++) {
+  for (let j = 0; j < arr[i].length; j++) {
+    product *= arr[i][j];
+  }
+}
+  // Only change code above this line
+  return product;
+}
+
+multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);//Nesting For Loops
