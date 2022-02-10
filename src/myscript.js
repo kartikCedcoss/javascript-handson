a60b80d92109c90c6c968187c041c7d59a0d88a5
Kartik saini <kartiksaini@cedcommerce.com>
2022-02-10 09:13:01 +0530

    Accessing Nested Arrays

--- src/myscript.js
+++ src/myscript.js
@@ -883,3 +883,24 @@ const myStorage = {
 };
 
 const gloveBoxContents = myStorage.car.inside["glove box"];//Accessing Nested Objects
+
+const myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+const secondTree = myPlants[1].list[1];//Accessing Nested Arrays
