91584215cb1ed288e3ce52d3465b0845dde235fe
Kartik saini <kartiksaini@cedcommerce.com>
2022-02-10 09:38:31 +0530

    Count Backwards With a For Loop

--- src/myscript.js
+++ src/myscript.js
@@ -930,3 +930,11 @@ const myArray = [];
 for(let i=1; i<=9; i+=2){
   myArray.push(i)
 }//Iterate Odd Numbers With a For Loop
+
+// Setup
+const myArray = [];
+
+// Only change code below this line
+for(let i=9; i>0; i-=2){
+  myArray.push(i)
+}//Count Backwards With a For Loop
