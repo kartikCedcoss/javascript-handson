d32dcf104b7316691cdfcb490e95d877d69cef00
Kartik saini <kartiksaini@cedcommerce.com>
2022-02-10 08:52:32 +0530

    Accessing Object Properties with Bracket Notation

--- src/myscript.js
+++ src/myscript.js
@@ -769,3 +769,14 @@ const testObj = {
   // Only change code below this line
   const hatValue = testObj.hat;      // Change this line
   const shirtValue = testObj.shirt;    // Accessing Object Properties with Dot Notation
+
+   // Setup
+const testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+const entreeValue = testObj["an entree"];   // Change this line
+const drinkValue = testObj["the drink"];  //Accessing Object Properties with Bracket Notation
